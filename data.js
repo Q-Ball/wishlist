c92a86c33970d135dafb775dfc49158ddaff40f9
Nick Drozdovskiy <drozdovskiyN@gmail.com>
2023-09-25 19:22:54 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,3 @@
 export let items = [
-    {"title":"Xiaomi Mi Robot Vacuum Mop", "description":"Пылесос-робот Xiaomi Mi Robot Vacuum Mop − функциональный бытовой прибор, с которым вы добьетесь идеальной чистоты в доме. Особенность устройства в поддержке влажной и сухой уборки, что позволит использовать его и для сбора мусора, и для мытья пола.", "price":"16099", "link" : "https://www.dns-shop.ru/product/189951624b9e1b80/pylesos-robot-xiaomi-mi-robot-vacuum-mop-belyj/", "image_link" : "https://c.dns-shop.ru/thumb/st1/fit/wm/0/0/c17f7cd2901183169a6a2916a2812b62/8fa3f7356b859a7fdf6e7dff9ebbc3e6ccf211e245e7059eb0115b5f94799230.jpg"},
-    //{"title":"", "description":"", "price":"", "link" : "", "image_link" : ""},
+    {"title":"", "description":"", "price":"", "link" : "", "image_link" : ""},
 ];
